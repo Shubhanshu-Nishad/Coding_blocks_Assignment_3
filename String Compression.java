2b0dd297d273471f8cbc3686a79dcfb9e65ed3c7
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2022-09-21 21:59:53 +0530

    Create String Compression.java

---
+++ String Compression.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+        int count=0;
+		for(int i = 0;i<str.length()-1;i++){
+			if( (int) str.charAt(i) == (int) str.charAt(i+1))
+				count++;
+			else{
+                System.out.print(str.charAt(i)+""+(count+1));
+                count=0;
+            }
+		}
+        System.out.print(str.charAt(str.length()-1)+""+(count+1));	
+    }
+}
+// https://hack.codingblocks.com/app/contests/3539/380/problem
