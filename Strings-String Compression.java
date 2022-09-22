4d9c2a7d065d39420605f1d80b42042a4c9e7d66
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2022-09-22 09:44:33 +0530

    Update Strings-String Compression.java

--- Strings-String Compression.java
+++ Strings-String Compression.java
@@ -3,20 +3,25 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 		String str = sc.next();
-        int count=0;
+        int count=1;
 		for(int i = 0;i<str.length()-1;i++){
 			if( (int) str.charAt(i) == (int) str.charAt(i+1))
 				count++;
 			else{
-				if(count>0){
-					System.out.print(str.charAt(i)+""+(count+1));
+				if(count>1){
+					System.out.print(str.charAt(i)+""+(count));
 				}
-                count=0;
+				else
+					System.out.print(str.charAt(i));
+                count=1;
             }
 		}
-		if(count>0)
-			System.out.print(str.charAt(str.length()-1)+""+(count+1));	
+		if(count>1)
+			System.out.print(str.charAt(str.length()-1)+""+(count));
+			else
+			System.out.print(str.charAt(str.length()-1));
+				
     }
-} 
+}
 
 // https://hack.codingblocks.com/app/contests/3539/468/problem
